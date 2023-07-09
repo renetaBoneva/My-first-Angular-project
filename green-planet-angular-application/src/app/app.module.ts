d50e8d7a059ca942951a405b5690800c7bfc6fad
renetaBoneva <reneta.boneva@abv.bg>
2023-07-09 17:45:39 +0300

    Working on app setup. Adding navigation and footer

--- green-planet-angular-application/src/app/app.module.ts
+++ green-planet-angular-application/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
