303ac3aecc27adadcf227e3ee17a094221cfcc82
renetaBoneva <reneta.boneva@abv.bg>
2023-07-25 15:35:07 +0300

    Adding logout functionality

---
+++ green-planet-angular-application/src/app/features/user/pages/logout/logout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user-service.service';
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.component.html',
+  styleUrls: ['./logout.component.css']
+})
+export class LogoutComponent implements OnInit{
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.logout();
+  }
+}
