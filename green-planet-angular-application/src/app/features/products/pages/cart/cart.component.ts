7816aee329cbcb88f3b996a007360a0be3b6f148
renetaBoneva <reneta.boneva@abv.bg>
2023-07-25 17:42:29 +0300

    Reading unlogged user's products on cart page

--- green-planet-angular-application/src/app/features/products/pages/cart/cart.component.ts
+++ green-planet-angular-application/src/app/features/products/pages/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderProduct } from '../../types/OrderProduct';
 import { environment } from 'src/environments/environment.development';
+import { UserService } from 'src/app/features/user/services/user-service.service';
 
 @Component({
   selector: 'app-cart',
@@ -12,16 +13,34 @@ export class CartComponent implements OnInit {
   areProducts: boolean = false;
   orderTotal: number = 0;
 
-  ngOnInit(): void {
-    const lsData = localStorage.getItem(environment.USER_KEY_LOCAL_STORAGE);
+  constructor(private userService: UserService) { }
 
-    if (lsData) {
-      const lsJSON = JSON.parse(lsData);
-      this.cartProducts = lsJSON.myCart;
-      this.cartProducts.length > 0
-        ? this.areProducts = true
-        : this.areProducts;
+  ngOnInit(): void {
+    this.getCartProductsLocalStorage()
+  }
+
+  getCartProductsLocalStorage() {
+    if (this.userService.isLogged) {
+      // for logged users
+      const lsData = localStorage.getItem(environment.USER_KEY_LOCAL_STORAGE);
+
+      if (lsData) {
+        const lsJSON = JSON.parse(lsData);
+        this.cartProducts = lsJSON.myCart;
+      }
     }
+    else {
+      // for unlogged users
+      const lsData = localStorage.getItem(environment.CART_KEY_LOCAL_STORAGE);
+      if (lsData) {
+        const lsJSON = JSON.parse(lsData);
+        this.cartProducts = lsJSON;
+      }
+    }
+
+    this.cartProducts.length > 0
+      ? this.areProducts = true
+      : this.areProducts;
 
     if (this.areProducts) {
       this.cartProducts.map(p => this.orderTotal += p.price * p.count);
