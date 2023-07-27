7b5501c51d19019648c3f55e5ecc9897aec1d1ca
renetaBoneva <reneta.boneva@abv.bg>
2023-07-27 16:35:16 +0300

    Add loading component

--- green-planet-angular-application/src/app/app-routing.module.ts
+++ green-planet-angular-application/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LandingPageComponent } from './features/products/pages/landing-page/landing-page.component';
+import { LoadingComponent } from './shared/components/loading/loading.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', component: LandingPageComponent},
   {path: 'home', component: LandingPageComponent},
+  {path: 'loading', component: LoadingComponent},
 ]
 
 @NgModule({
--- green-planet-angular-application/src/app/features/products/pages/product-details/product-details.component.ts
+++ green-planet-angular-application/src/app/features/products/pages/product-details/product-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { db } from 'src/db.module';
 import { ProductDetails } from '../../types/ProductDetails';
 
@@ -12,16 +12,22 @@ export class ProductDetailsComponent implements OnInit {
   id: number | null = null;
   product: ProductDetails | null = null;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.getProductDetails();
   }
-  
+
   getProductDetails() {
     this.id = this.activatedRoute.snapshot.params['id'];
-    
+
     // Temporary db TODO: get data from api service
     this.product = db.products.filter(product => product._id == this.id)[0];
+    if (!this.product) {
+      this.router.navigate(['/error'])
+    }
   }
 }
