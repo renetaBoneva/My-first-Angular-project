77bff8c4cdf3085db3d5b4fdb8b38257b48debd6
renetaBoneva <reneta.boneva@abv.bg>
2023-08-05 17:41:19 +0300

    Add filter type interface

---
+++ green-planet-angular-application/src/app/features/products/types/Filters.ts
@@ -0,0 +1,12 @@
+export interface Filters {
+  category: {
+    flowers: string,
+    outdoor: string,
+    indoor: string,
+    trees: string
+  },
+  price: {
+    priceFrom: string,
+    priceTo: string
+  }
+}
