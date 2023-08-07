3ba316fbb37221199721cfdb3d1d6e4cf584b82d
renetaBoneva <reneta.boneva@abv.bg>
2023-08-07 11:34:02 +0300

    Fixing profile edit

--- green-planet-angular-application/src/app/features/user/pages/profile/profile.component.ts
+++ green-planet-angular-application/src/app/features/user/pages/profile/profile.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserDetails } from '../../types/UserDetails';
 import { UserService } from '../../services/user-service.service';
 import { Order } from 'src/app/features/products/types/Order';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -12,32 +12,51 @@ import { NgForm } from '@angular/forms';
 })
 export class ProfileComponent implements OnInit {
   userInfo: UserDetails | undefined;
-  userOrders: Order[] | undefined;
+  userOrders: Order[] = [];
   isEditMode: boolean = false;
-  
-  constructor(private userService: UserService) { }
-  
+
+  form = this.fb.group({
+    firstName: ["", [Validators.required, Validators.minLength(2)]],
+    lastName: ["", [Validators.required, Validators.minLength(2)]],
+    address: ["", [Validators.required, Validators.minLength(4)]],
+  })
+
+  constructor(
+    private userService: UserService,
+    private fb: FormBuilder,
+  ) { }
+
   ngOnInit(): void {
     this.userService.getUserDetails()
     this.userService.userDetails$.subscribe({
       next: userInfo => {
-        console.log(userInfo);
-        
+        if (userInfo) {
+          this.form.setValue({
+            firstName: userInfo?.firstName,
+            lastName: userInfo?.lastName,
+            address: userInfo?.address,
+          })
+        }
         this.userInfo = userInfo;
-        this.userOrders = userInfo?.myOrders;
+        this.userOrders = userInfo ? userInfo.myOrders : [];
       }
-    })    
+    })
   }
 
-  editUser(form: NgForm) {
-    if (form.invalid) {
+
+  editUserReact() {
+    if (this.form.invalid) {
       return;
     }
 
-    const { firstName, lastName, address } = form.value;
-    this.userService.editUser(firstName, lastName, address,this.userInfo?._id)
-    this.changeMode();
+    const { firstName, lastName, address } = this.form.value;
+    
+    if (firstName && lastName && address) {
+      this.userService.editUser({ ...this.userInfo!, firstName, lastName, address })
+      this.changeMode();
+    }
   }
+
 
   deleteUser() {
     const areYouSure = window.confirm('Are you sure you want to delete your profile?')
@@ -48,8 +67,14 @@ export class ProfileComponent implements OnInit {
   }
 
   cancelEdit() {
-    // TODO: make it reactive form
-    this.userService.getUserDetails()
+    if (this.userInfo) {
+      this.form.setValue({
+        firstName: this.userInfo?.firstName,
+        lastName: this.userInfo?.lastName,
+        address: this.userInfo?.address,
+      })
+    }
+
     this.changeMode();
   }
 
